1607b631e6d9589e9bc8e987aeaa74b15cd06847
Andrew Jabbour <ajabbour@Sultans-MacBook-Pro.local>
2018-01-28 12:05:11 -0500

    Commit

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,5 @@
-alert(1);
+document.getElementById("myButton").addEventListener("click", myFunction);
+
+function myFunction(){
+    document.getElementById("helloMessage").innerHTML = "Hello " + document.getElementById("nameEntry").value +"!";
+}
